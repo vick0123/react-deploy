0861f0d1fa3445c188be01c31b45d186790bd1cd
Vivek Patel <31905378+vick0123@users.noreply.github.com>
2020-10-15 13:20:59 -0400

    Updates

---
+++ precache-manifest.ecef04050cad55f95cb3fdd79d1d0c85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38974f2d6225b3e5352f7c06c2b0b2f2",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "959c845cdaa47e6c5e3a",
+    "url": "/react-deploy/static/css/main.6a9e3ab4.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "959c845cdaa47e6c5e3a",
+    "url": "/react-deploy/static/js/main.71fe3a69.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "c9177d4b17fe12ede47e1667df56602e",
+    "url": "/react-deploy/static/media/Vivekimg.c9177d4b.jpg"
+  }
+]);
